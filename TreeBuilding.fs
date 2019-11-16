4018b1d8baa9b49d03cc44bf00b34ddd1339adcd
Scott Hutchinson <scotthutchinson@usa.net>
2019-11-16 11:20:18 -0800

    Moving type definitions for new version

--- TreeBuilding.fs
+++ TreeBuilding.fs
@@ -1,5 +1,25 @@
 ﻿module TreeBuilding
+
 open TreeBuildingTypes
+
+type Tree =
+    | Branch of int * Tree list
+    | Leaf of int
+
+let recordId t =
+    match t with
+    | Branch (id, _) -> id
+    | Leaf id -> id
+
+let isBranch t =
+    match t with
+    | Branch _ -> true
+    | Leaf _ -> false
+
+let children t =
+    match t with
+    | Branch (_, c) -> c
+    | Leaf _ -> []
 
 let rec build prev acc list =
     match list with
